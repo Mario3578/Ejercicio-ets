c28a69fe63d7b8f76393c5e8872b400d49edc82f
admon <admon@DESKTOP-F5I7FMH.mshome.net>
2021-03-03 19:10:57 +0000

    Paso 1 y 2

---
+++ src/main/java/com/mycompany/calculadorets/Calculator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.calculadorets;
+
+/**
+ *
+ * @author admon
+ */
+public class Calculator {
+    
+    private String operador1;
+    
+    private String operador2;
+    
+    private String operacion;
+    
+    
+    
+}
